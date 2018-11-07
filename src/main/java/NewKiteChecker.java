97396224ed19d575b7b1e5b2423d198151029594
sahjan <sahjan.soomro@gmail.com>
2018-11-07 16:32:39 +0000

    added comments

--- src/main/java/NewKiteChecker.java
+++ src/main/java/NewKiteChecker.java
@@ -6,11 +6,15 @@ public class NewKiteChecker {
     private ArrayList<Kite> newKites;
 
     public List<Kite> checkForNewKites(List<Kite> changeTrackerDocKites, List<Kite> changeQueueKites) {
-
         newKites = new ArrayList<>();
-        //DO CHECKING LOGIC and add new kites to newKites arraylist
-        newKites.add(kite);
 
+        //DO CHECKING LOGIC and add new kites to newKites arraylist
+        for (Kite k : changeQueueKites) {
+            //check if present in change tracker kites.
+            //if not, then it is a new kite, then add into newKites.
+            newKites.add(kite);
+        }
+        
         return newKites;
     }
 }
